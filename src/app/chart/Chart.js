d6e94c7f319bfc6cc3157d49f49b97998673b478
Taras Serdyuk <taras@techburg.io>
2015-07-29 22:51:06 +0300

    improved chart

--- src/app/chart/Chart.js
+++ src/app/chart/Chart.js
@@ -4,34 +4,36 @@ export default class Chart extends React.Component {
 	componentDidMount() {
 		const bl = 4.8
 		const { points } = this.props
-		$(function() {
-			$.plot('#chart', [{
-				data: points,
-				color: 'rgb(30, 180, 20)',
-				threshold: {
-					below: bl,
-					color: 'rgb(200, 20, 30)'
-				},
-				lines: { show: true },
-				curvedLines: {
-					apply: true,
-					monotonicFit: true
-				}
-			}, {
-				data: [
-					[points[0][0], bl],
-					[points[points.length - 1][0], bl]
-				],
-				color: 'red',
-				lines: { show: true }
-			}, {
-				data: points,
-				points: { show: true },
-			}], {
-    			series: {
-    				curvedLines: { active: true }
-    			}
-			})
+		$.plot('#chart', [{
+			data: points,
+			color: 'rgb(30, 180, 20)',
+			threshold: {
+				below: bl,
+				color: 'rgb(200, 20, 30)'
+			},
+			lines: { show: true },
+			curvedLines: {
+				apply: true,
+				monotonicFit: true
+			}
+		}, {
+			data: [
+				[points[0][0], bl],
+				[points[points.length - 1][0], bl]
+			],
+			color: 'red',
+			lines: { show: true }
+		}, {
+			data: points,
+			points: { show: true },
+		}], {
+			yaxis: {
+				ticks: 10,
+				tickDecimals: 0
+			},
+			series: {
+				curvedLines: { active: true }
+			}
 		})
 	}
 	render() {
